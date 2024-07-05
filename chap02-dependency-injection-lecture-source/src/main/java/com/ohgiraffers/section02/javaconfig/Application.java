6684e1f19bdce3207bf42b49909840114c3cbd06
HEun0420 <usialeta@gmail.com>
2024-07-05 10:54:37 +0900

    dependency

---
+++ chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.section02.javaconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+        // XML 설정 파일을 기반으로 ApplicationContext 생성
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        MemberDTO member = context.getBean(MemberDTO.class);
+
+        System.out.println(member);
+
+        // MemberDTo의 personalAccount 객체 출력
+        System.out.println(member.getPersonalAccount());
+
+        // 10000원 입금
+        System.out.println(member.getPersonalAccount().deposit(10000));
+
+        // 잔액 출력
+        System.out.println(member.getPersonalAccount().getBalance());
+
+        // 5000원 출금
+        System.out.println(member.getPersonalAccount().withdraw(5000));
+
+        // 잔액 출력
+        System.out.println(member.getPersonalAccount().getBalance());
+    }
+}
