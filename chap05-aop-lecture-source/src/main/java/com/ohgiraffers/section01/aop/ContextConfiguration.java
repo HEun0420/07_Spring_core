57acaa89573a024a97ffb93d0e31ab0b9c82236e
HEun0420 <usialeta@gmail.com>
2024-07-08 16:28:22 +0900

    aop

--- chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfiguration.java
+++ chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfiguration.java
@@ -1,7 +1,12 @@
 package com.ohgiraffers.section01.aop;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @Configuration
 public class ContextConfiguration {
+    public ContextConfiguration() {
+    }
 }
